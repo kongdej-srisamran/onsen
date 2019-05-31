6796ca6dde8f9f569222d16fd3bc80e62878d5c8
kongdej <kongdejs@gmail.com>
2019-05-31 23:34:55 +0700

    add esp32 code

---
+++ esp32/onsen/onsen.ino
@@ -0,0 +1,206 @@
+#include <BLEServer.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 15
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+#define DEVICE_NAME "Onsen Maker"
+//#define USER_SERVICE_UUID "f2e9df0a-7899-4505-8ac1-e19c467ade7f"
+#define USER_SERVICE_UUID "5fe83910-3522-44e3-85c9-13f9b017b72f"
+
+#define WRITE_CHARACTERISTIC_UUID "E9062E71-9E62-4BC6-B0D3-35CDCD9B027B"
+#define NOTIFY_CHARACTERISTIC_UUID "62FBD229-6EDD-4D1A-B554-5C4E1BB29169"
+
+#define PSDI_SERVICE_UUID "e625601e-9e55-4597-a598-76018a0d293d"
+#define PSDI_CHARACTERISTIC_UUID "26e2b12b-85f0-4f3f-9fdd-91d114270e6e"
+
+"serviceUuid":"5fe83910-3522-44e3-85c9-13f9b017b72f",
+"psdiServiceUuid":"e625601e-9e55-4597-a598-76018a0d293d",
+"psdiCharacteristicUuid":"26e2b12b-85f0-4f3f-9fdd-91d114270e6e"}
+
+
+#define RELAY 2
+#define ON 0
+#define OFF 1
+
+unsigned int timeout=0;
+unsigned int low=60;
+unsigned int high=70;
+unsigned int cmd=0;
+
+BLEServer* thingsServer;
+BLESecurity *thingsSecurity;
+BLEService* userService;
+BLEService* psdiService;
+BLECharacteristic* psdiCharacteristic;
+BLECharacteristic* writeCharacteristic;
+BLECharacteristic* notifyCharacteristic;
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+class serverCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+   deviceConnected = true;
+  };
+
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
+};
+
+class writeCallback: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *bleWriteCharacteristic) {
+    std::string value = bleWriteCharacteristic->getValue();
+
+    String str;
+    
+    for (uint8_t i = 0; i < value.length(); i++) {
+        str += (char)value[i];
+    }   
+    Serial.println(str);
+    
+    String ts = getValue(str, ',', 0);
+    timeout = ts.toInt();
+    String tl = getValue(str, ',', 1);
+    low = tl.toInt();
+    String th = getValue(str, ',', 2);
+    high = th.toInt();
+    String c = getValue(str, ',', 3);
+    cmd = c.toInt();
+    
+  }
+  
+  String getValue(String data, char separator, int index) {
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+  }
+};
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(RELAY, OUTPUT);
+  digitalWrite(RELAY, OFF);
+
+  BLEDevice::init("");
+  BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT_NO_MITM);
+
+  // Security Settings
+  BLESecurity *thingsSecurity = new BLESecurity();
+  thingsSecurity->setAuthenticationMode(ESP_LE_AUTH_REQ_SC_ONLY);
+  thingsSecurity->setCapability(ESP_IO_CAP_NONE);
+  thingsSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
+
+  setupServices();
+  startAdvertising();
+  Serial.println("Ready to Connect");
+  sensors.begin();
+}
+
+void loop() {  
+  char temperature[100];
+
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  float tempC = sensors.getTempCByIndex(0);   //tempC = random(100, 500) / 10.0;  //Simulation
+  timeout = timeout > 0 ? timeout-1 : 0;
+  if (timeout == 0) {
+    digitalWrite(RELAY, OFF);
+  }
+  else {
+    if (tempC <= low)  {
+      Serial.println("Start by low");
+      digitalWrite(RELAY, ON);    
+    }
+  }
+  
+  if (tempC >= high)  {
+    Serial.println("Stop with high");
+    digitalWrite(RELAY, OFF);
+  }
+    
+  int relaystatus = digitalRead(RELAY);
+  sprintf(temperature,"%0.2f,%d,%d", tempC,timeout,relaystatus);  
+  Serial.print(timeout);Serial.print(")");Serial.print(temperature);Serial.print(" --- ");Serial.print(low);Serial.print(">");Serial.print(high);
+  Serial.print("-");Serial.print(cmd);Serial.print("-");Serial.println(relaystatus);
+    
+  notifyCharacteristic->setValue(temperature);
+  notifyCharacteristic->notify();
+  
+  delay(20);
+  
+  // Disconnection
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500); // Wait for BLE Stack to be ready
+    thingsServer->startAdvertising(); // Restart advertising
+    oldDeviceConnected = deviceConnected;
+    Serial.println("Disconnection & restart advertising");
+  }
+  // Connection
+  if (deviceConnected && !oldDeviceConnected) {
+    oldDeviceConnected = deviceConnected;
+    Serial.println("connected");
+  }
+
+   delay(500);
+}
+
+void setupServices(void) {
+  // Create BLE Server
+  thingsServer = BLEDevice::createServer();
+  thingsServer->setCallbacks(new serverCallbacks());
+
+  // Setup User Service
+  userService = thingsServer->createService(USER_SERVICE_UUID);
+  // Create Characteristics for User Service
+  writeCharacteristic = userService->createCharacteristic(WRITE_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE);
+  writeCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+  writeCharacteristic->setCallbacks(new writeCallback());
+
+  notifyCharacteristic = userService->createCharacteristic(NOTIFY_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+  notifyCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+  BLE2902* ble9202 = new BLE2902();
+  ble9202->setNotifications(true);
+  ble9202->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+  notifyCharacteristic->addDescriptor(ble9202);
+
+  // Setup PSDI Service
+  psdiService = thingsServer->createService(PSDI_SERVICE_UUID);
+  psdiCharacteristic = psdiService->createCharacteristic(PSDI_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ);
+  psdiCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+
+  // Set PSDI (Product Specific Device ID) value
+  uint64_t macAddress = ESP.getEfuseMac();
+  psdiCharacteristic->setValue((uint8_t*) &macAddress, sizeof(macAddress));
+
+  // Start BLE Services
+  userService->start();
+  psdiService->start();
+}
+
+void startAdvertising(void) {
+  // Start Advertising
+  BLEAdvertisementData scanResponseData = BLEAdvertisementData();
+  scanResponseData.setFlags(0x06); // GENERAL_DISC_MODE 0x02 | BR_EDR_NOT_SUPPORTED 0x04
+  scanResponseData.setName(DEVICE_NAME);
+
+  thingsServer->getAdvertising()->addServiceUUID(userService->getUUID());
+  thingsServer->getAdvertising()->setScanResponseData(scanResponseData);
+  thingsServer->getAdvertising()->start();
+}
